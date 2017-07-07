01337a09a196917e600e8495c46aaa39d4eb7bdb
yugiohking <yugioh1627752@gmail.com>
2017-07-07 19:01:02 +0800

    Add files via upload

--- test.php
+++ test.php
@@ -2,45 +2,7 @@
 <html>
 <head>
 	<link rel="stylesheet" href="style.css">
-	<title>Yeah!!!<<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="style.css">
 	<title>Yeah!!!</title>
-</head>
-<form action="./test.php">
-	<fieldset>
-		<legend>Registration</legend>
-			Name:<br>
-				<input type="text" name="name"><br>
-			Email:<br>
-				<input type="text" name="email"><br>
-			Password:<br>
-				<input type="text" name="password"><br>
-			Birth:<br>
-				<select name="birth">
-					<option value="2003">2003</option>
-				</select>
-				
-				<select name="birth">
-					<option value="January">January</option>
-					<option value="January">February</option>
-				</select>
-				
-				<select name="birth">
-					<option value="1">1</option>
-				</select>
-			Gender:<br>
-			<input type="radio" name="gender" value="Male">Male
-			<input type="radio" name="gender" value="Female">Female
-			<br><br>
-			<input type="submit">
-	</fieldset>
-</form>
-
-<footer>Copyright &copy; END_by_Terminator</footer>
-</body>
-</html>/title>
 </head>
 
 <h1><b><i>END<br>(created by <abbr title="Terminator">Terminator</abbr>)</i></b></h1>
